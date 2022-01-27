d4cd693b2a06f219c903b6b0378e868adaefacbc
Patrick Quilty <pquilty79@gmail.com>
2022-01-27 16:52:10 -0600

    updated today's exercises

--- code.js
+++ code.js
@@ -1,53 +1,66 @@
-// helloWorld function 
-function helloWorld() {
-    return "Hello, World!";
-}
-// function expression version:
-// const helloWorld = function() {
+// // helloWorld function
+// function helloWorld() {
 //     return "Hello, World!";
 // }
+// // function expression version:
+// // const helloWorld = function() {
+// //     return "Hello, World!";
+// // }
+//
+// function sayHello(input) {
+//     return ("Hello, " + input + "!");
+// }
+//
+// sayHello("Jane")
+// sayHello("Alex")
+// sayHello("Pat")
+//
+// function isFive(number) {
+//     if (number === 5) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+//
+//
+// function isEven(number) {
+//     if (typeof number == "boolean") {
+//         return false;
+//     } else if (number % 2 === 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+//
+//
+// function isVowel(letter) {
+//     const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+//     return vowels.includes(letter);
+// }
+//
+//
+//
+// function add(a, b) {
+//     if (typeof (a + b) !== "number") {
+//         return isNaN(NaN);
+//     } else {
+//         return (a + b);
+//     }
+// }
+//
+// console.log(add())
 
-function sayHello(input) {
-    return ("Hello, " + input + "!");
+ let i = 0;
+// while (i < 24) {
+//     i = i + 2
+//     console.log(i);
+// }
+
+// or
+while (i <= 25){
+    if (i % 2 ===0){
+        console.log(i)
+    } i++
 }
-
-sayHello("Jane")
-sayHello("Alex")
-sayHello("Pat")
-
-function isFive(number) {
-    if (number === 5) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-
-function isEven(number) {
-    if (typeof number == "boolean") {
-        return false;
-    } else if (number % 2 === 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-
-function isVowel(letter) {
-    const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
-    return vowels.includes(letter);
-}
-
-
-
-function add(a, b) {
-    if (typeof (a + b) !== "number") {
-        return isNaN(NaN);
-    } else {
-        return (a + b);
-    }
-}
-
-console.log(add())
